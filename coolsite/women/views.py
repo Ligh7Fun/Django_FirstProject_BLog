5190d86c6fb64e7bd8ad481ef2a4af4839b21a69
Ligh7Fun <m5221710@gmail.com>
2023-03-10 14:39:29 +0500

    del HttpResponseNotFound

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404, HttpResponseNotFound
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Women, Category
 
