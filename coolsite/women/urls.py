bfd52ce976bd07923f22ab683bfb602c686e833e
Ligh7Fun <m5221710@gmail.com>
2023-03-10 08:59:35 +0500

    first commit

---
+++ coolsite/women/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path  # , re_path
+from .views import index, about, addpage, contact,\
+    login, show_category, show_post
+
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<slug:post_slug>/', show_post, name='post'),
+    path('category/<slug:cat_slug>/', show_category, name='category'),
+    # re_path(r'^archive/(?P<year>[0-9]{4})', archive),
+]
---
+++ coolsite/women/views.py
@@ -0,0 +1,82 @@
+from django.http import Http404, HttpResponseNotFound
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Women, Category
+
+
+def index(request):
+    posts = Women.objects.all()
+    context = {
+        'posts': posts,
+        'cat_selected': 0,
+    }
+
+    return render(request, 'women/index.html', context=context)
+
+
+def about(request):
+    context = {
+    }
+
+    return render(request, 'women/about.html', context=context)
+
+
+def addpage(request):
+    context = {
+    }
+
+    return render(request, 'women/addpage.html', context=context)
+
+
+def contact(request):
+    context = {
+    }
+
+    return render(request, 'women/contact.html', context=context)
+
+
+def login(request):
+    context = {
+    }
+
+    return render(request, 'women/login.html', context=context)
+
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    context = {
+        'post': post,
+        'title': 'Посты',
+        # 'cat_selected': post.cat,
+    }
+
+    return render(request, 'women/post.html', context=context)
+
+
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(cat_id__slug=cat_slug)
+    cats = Category.objects.filter(slug=cat_slug)
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'cat_selected': cat_slug,
+    }
+
+    return render(request, 'women/index.html', context=context)
+
+
+def archive(request, year):
+    context = {
+    }
+
+    if int(year) > 2023:
+        return redirect('home', permanent=False)
+
+    return render(request, 'women/archive.html', context=context)
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>404 страница не найдена</h1>')
